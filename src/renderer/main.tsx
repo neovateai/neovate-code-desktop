8f94ac5019064bb847761b77ad9c82e7f59962c8
Din <iamdinq@gmail.com>
2026-01-29 11:05:50 +0800

    feat: add renderer app abstraction with plugin system (#51)

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
-import { useStore } from './store';
-import { hydrateStore, setupPersistence } from './persistence';
-import { ToastProvider } from './components/ui/toast';
+import { RendererApp } from './core';
 
-// Hydrate store from persisted state before rendering
-(async () => {
-  await hydrateStore(useStore);
+const rendererApp = new RendererApp();
 
-  // Render the app
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <ToastProvider position="bottom-right">
-        <App />
-      </ToastProvider>
-    </React.StrictMode>,
-  );
-
-  // Setup automatic persistence after rendering
-  setupPersistence(useStore);
-})();
+rendererApp.start();
