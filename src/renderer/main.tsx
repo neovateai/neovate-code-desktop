6972f0288733c815bb7700fc826e88c46628d9c4
sorrycc <sorrycc@gmail.com>
2026-02-09 17:30:39 +0800

    chore: remove demo dashboard plugin from renderer

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,13 +1,9 @@
 import './index.css';
 import { RendererApp } from './core';
-import {
-  demoDashboardPlugin,
-  demoDashboardWindowConfig,
-} from './plugins/demo-dashboard/plugin';
 
 const rendererApp = new RendererApp({
-  plugins: [demoDashboardPlugin],
-  windows: [demoDashboardWindowConfig],
+  plugins: [],
+  windows: [],
 });
 
 rendererApp.start();
