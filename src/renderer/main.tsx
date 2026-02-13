71de4567e42625c609620922a30e1916b738009a
sorrycc <sorrycc@gmail.com>
2026-02-13 14:47:00 +0800

    chore: remove demo plugin and window configuration

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,15 +1,9 @@
 import './index.css';
 import { RendererApp } from './core';
-import { demoPlugin } from './plugins/demo';
 
 const rendererApp = new RendererApp({
-  plugins: [demoPlugin],
-  windows: [
-    {
-      windowType: 'demo',
-      componentLoader: () => import('./plugins/demo/DemoWindow'),
-    },
-  ],
+  plugins: [],
+  windows: [],
 });
 
 rendererApp.start();
